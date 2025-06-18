d164dacd61a398cffa4a97f43ec6c1b6a7afe1f6
Aman Khan <140867904+aman-khan001@users.noreply.github.com>
2025-06-18 10:18:32 +0530

    settings updated

--- mansoon1/settings.py
+++ mansoon1/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# https://docs.djangoproject.com/en/5.1/howto/static-files/ 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
